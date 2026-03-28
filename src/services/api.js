09e5c934d592a22939b19c43c6218d2fd037e59e
geadolesapp <geadolesapp@gmail.com>
2026-03-28 18:18:53 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+const baseURL =
+  window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+    ? "http://localhost:5000/api"
+    : "https://teleios-bank-back.onrender.com/api";
 
 const api = axios.create({
   baseURL,

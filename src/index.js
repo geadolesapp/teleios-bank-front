7676b8ea5f7d56b35d263f67aa35392414fb99c5
geadolesapp <geadolesapp@gmail.com>
2026-03-29 15:35:00 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { register } from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -10,4 +10,4 @@ root.render(
   <App />
 );
 
-serviceWorkerRegistration.register();
+register();
